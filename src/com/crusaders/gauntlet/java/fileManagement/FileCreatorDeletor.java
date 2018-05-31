3e42e92ad37cb9a0e083dc86877028ace2e18d6e
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2018-05-31 17:34:34 +0300

    Cleared all files

--- src/com/crusaders/gauntlet/java/fileManagement/FileCreatorDeletor.java
+++
@@ -1,9 +0,0 @@
-package com.crusaders.gauntlet.java.fileManagement;
-
-import java.io.File;
-
-public interface FileCreatorDeletor {
-    void delete (String name, String path);
-    void createDirectory(String directoryPath);
-	void createFile(File file, String dirPath);
-}
