d7917b8e72d178eaa01852f1dbe53a710bf24e95
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2018-08-16 17:02:38 +0300

    Tweaked the writers classes so that we can append at the end of the file if we want to

--- src/com/crusaders/gauntlet/java/fileManagement/writing/IFileWriter.java
+++ src/com/crusaders/gauntlet/java/fileManagement/writing/IFileWriter.java
@@ -1,4 +1,4 @@
-package com.crusaders.gauntlet.java.fileManagement.writing;
+package com.crewgame.fileManagement.writing;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,16 +11,18 @@ public interface IFileWriter
      * 
      * @param file to be written to
      * @param string to be written
+     * @param whether or not it should append at the end of the file
      * @throws IOException
      */
-    void writeToFile(File file, String str) throws IOException;
+    void writeToFile(File file, String str, boolean append) throws IOException;
     /**
      * 
      * Writes to the file annotated by the path
      * 
      * @param path to the file
      * @param string to be written
+     * @param whether or not it should append at the end of the file
      * @throws IOException
      */
-    void writeToFile(String path, String str) throws IOException;
+    void writeToFile(String path, String str, boolean append) throws IOException;
 }
--- src/com/crusaders/gauntlet/java/fileManagement/writing/Writer.java
+++ src/com/crusaders/gauntlet/java/fileManagement/writing/Writer.java
@@ -1,26 +1,26 @@
-package com.crusaders.gauntlet.java.fileManagement.writing;
+package com.crewgame.fileManagement.writing;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Writer implements IFileWriter {
 
     @Override
-    public void writeToFile(File file, String str) throws IOException 
+    public void writeToFile(File file, String str, boolean append) throws IOException 
     {
-        BufferedWriter writer = new BufferedWriter(new PrintWriter(file));
-        writer.write(str);
-        writer.close();
-        writer = null;
-
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file, append)));
+        out.print(str);
+        out.close();
+        out = null;
     }
 
     @Override
-    public void writeToFile ( String path , String str ) throws IOException 
+    public void writeToFile ( String path , String str, boolean append) throws IOException 
     {
-        writeToFile( new File(path) , str );
+        writeToFile( new File(path) , str, append );
         
     }
 }
