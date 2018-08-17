97c5f1ecf324f6743e157e56ec5d7c0121317134
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2018-08-17 19:31:24 +0300

    Added some security features to the Script class

--- src/com/crusaders/gauntlet/java/util/scriptManagement/Script.java
+++ src/com/crusaders/gauntlet/java/util/scriptManagement/Script.java
@@ -8,11 +8,36 @@ public class Script
     
     public Script(File fileScript)
     {
+        if(fileScript == null)
+        {
+            throw new IllegalArgumentException("The file assigned should not be null");
+        }else if(!fileScript.exists())
+        {
+            throw new IllegalArgumentException("The file assigned should exist");
+        }else if(!fileScript.isDirectory())
+        {
+            throw new IllegalArgumentException("The file assigned should be a file not a directory");
+        }
         this.fileScript = fileScript;
     }
     
     public File getScript()
     {
         return fileScript;
+    }
+    
+    public void setScript(File fileScript)
+    {
+        if(fileScript == null)
+        {
+            throw new IllegalArgumentException("The file assigned should not be null");
+        }else if(!fileScript.exists())
+        {
+            throw new IllegalArgumentException("The file assigned should exist");
+        }else if(!fileScript.isDirectory())
+        {
+            throw new IllegalArgumentException("The file assigned should be a file not a directory");
+        }
+        this.fileScript = fileScript;
     }
 }
